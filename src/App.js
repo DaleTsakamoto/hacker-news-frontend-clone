6d8fe66f169be1edc34149a6f941c7a37d90cc2f
dale.sakamoto <daletsakamoto@gmail.com>
2021-03-01 13:29:18 -0800

    more organizing of tags in Component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="main-page-container">
-      <div className="maing-page-body">
-      <header className="App-header">
+      <div className="main-page-center">
+        <div className='main-page-header-container'>
+          <h1>Hacker-News</h1>
+        </div>
+        <div className='main-page-body'>
         <h1>Hacker-News</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-      </header>
+        </div>
       </div>
     </div>
   );
