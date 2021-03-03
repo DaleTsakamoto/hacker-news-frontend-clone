82ba1cbf3ba86500c0957bdd64aef6dc0473fd07
dale.sakamoto <daletsakamoto@gmail.com>
2021-03-02 17:09:37 -0800

    making code modular beginning with header

---
+++ src/components/Header/index.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Header.css';
+
+function Header() {
+  return (
+    <div className='header-container'>
+      <a href='/'>
+        <img src={`../logo.png`} className='header-logo' alt='hacker-logo' />
+      </a>
+      <h1>Hacker News</h1>
+      <a className='header-subtitles' href='/new'>new</a>
+      <a className='header-subtitles' href='/past'>past</a>
+      <a className='header-subtitles' href='/comments'>comments</a>
+      <a className='header-subtitles' href='/ask'>ask</a>
+      <a className='header-subtitles' href='/show'>show</a>
+      <a className='header-subtitles' href='/jobs'>jobs</a>
+      <a className='header-subtitles-no-line' href='/submit'>submit</a>
+      <a id='header-login' href='/login'>login</a>
+    </div>
+  )
+}
+
+export default Header
--- src/components/Homepage/index.js
+++ src/components/Homepage/index.js
@@ -2,7 +2,9 @@ import React, { useState, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 
 import Footer from '../Footer'
+import Header from '../Header'
 import './Homepage.css';
+
 import * as storiesActions from '../../store/stories';
 
 function Homepage() {
@@ -27,20 +29,7 @@ useEffect(() => {
   return isLoaded && (
     <div className="main-page-container">
       <div className="main-page-center">
-        <div className='main-page-header-container'>
-          <a href='/'>
-            <img src={`../logo.png`} className='main-page-header-logo' alt='hacker-logo'/>
-          </a>
-          <h1>Hacker News</h1>
-          <a className='main-page-header-subtitles' href='/new'>new</a>
-          <a className='main-page-header-subtitles' href='/past'>past</a>
-          <a className='main-page-header-subtitles' href='/comments'>comments</a>
-          <a className='main-page-header-subtitles' href='/ask'>ask</a>
-          <a className='main-page-header-subtitles' href='/show'>show</a>
-          <a className='main-page-header-subtitles' href='/jobs'>jobs</a>
-          <a className='main-page-header-subtitles-no-line' href='/submit'>submit</a>
-          <a id='main-page-header-login' href='/login'>login</a>
-        </div>
+        <Header />
         <div className='main-page-body'>
           <ol className='main-page-list-container'>
             {Object.values(topStories).map((sto, idx) => {
